8ebd3a83612da2cee9f61112e9a63a97cc41b854
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 17:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const search = document.querySelector(".search");
 const btn = document.querySelector(".btn")
 const input = document.querySelector(".input")
 
-
+btn.addEventListener("keypress", change);
 function change(){
-   search.addClassList.add("active");
+   search.classList.add("active");
 	input.focus();
 }
